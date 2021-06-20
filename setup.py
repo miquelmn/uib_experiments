778c4b284151e4946b560e5e000fb7c0424c3dcf
miquelmn <miquelca32@gmail.com>
2021-06-20 18:41:40 +0200

    v0.9.7.1 Added description

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+    README = f.read()
 
 with open('LICENSE') as f:
     LICENSE = f.read()
 
-setup(name='experiments',
-      version='0.9.7',
+setup(name='ugivia_experiments',
+      version='0.9.7.1',
       description='Handle the experiments.',
-      # long_description=README,
-      # long_description_content_type="text/markdown",
       url='https://gitlab.com/miquelca32/experiments',
       author='Miquel Miró Nicolau, Dr. Gabriel Moyà Alcover',
       author_email='miquel.miro@uib.cat, gabriel_moya@uib.es',
@@ -25,5 +23,7 @@ setup(name='experiments',
           'matplotlib',
           'scipy',
           'numpy'
-      ]
-      )
+      ],
+      long_description=README,
+      long_description_content_type="text/markdown",
+      zip_safe=False)
