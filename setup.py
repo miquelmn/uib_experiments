b21c122c461a98143ec67cfc4722a1f5854a0348
miquelmn <miquelca32@gmail.com>
2021-06-30 21:45:44 +0200

    v0.9.7.2 Now when keras experiment is finished it can send a plot

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ from setuptools import setup, find_packages
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    README = f.read()
 
 with open('LICENSE') as f:
     LICENSE = f.read()
 
 setup(name='ugivia_experiments',
-      version='0.9.7.1',
+      version='0.9.7.2',
       description='Handle the experiments.',
       url='https://gitlab.com/miquelca32/experiments',
       author='Miquel Miró Nicolau, Dr. Gabriel Moyà Alcover',
@@ -24,6 +22,4 @@ setup(name='ugivia_experiments',
           'scipy',
           'numpy'
       ],
-      long_description=README,
-      long_description_content_type="text/markdown",
       zip_safe=False)
