16ac35cc4246d3a32816ed8bb24034aea4412e22
Miquel Miró Nicolau <miquel.miro@uib.cat>
2022-12-12 22:06:47 +0100

    Setuptool updated to handle version 1.5.0

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ from setuptools import setup, find_packages
 LONG_DESCRIPTION = "Package to handle scientific experiments easily"
 
 setup(name='uib_experiments',
-      version='1.0.0',
+      version='1.5.0',
       description='Handle the experiments.',
       long_description=LONG_DESCRIPTION,
       url='https://github.com/miquelmn/uib_experiments',
       author='Miquel Miró Nicolau, Dr. Gabriel Moyà Alcover',
       author_email='miquel.miro@uib.cat, gabriel_moya@uib.es',
-      download_url = "https://github.com/miquelmn/uib_experiments/archive/refs/tags/v0.9.2.tar.gz",
+      download_url="https://github.com/miquelmn/uib_experiments/archive/refs/tags/v0.9.2.tar.gz",
       packages=find_packages(),
       install_requires=[
           'telegram_send',
           'opencv-python',
           'matplotlib',
           'scipy',
-          'numpy'
+          'numpy',
+          'peewee'
       ],
       zip_safe=False)
