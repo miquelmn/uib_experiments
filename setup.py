cce31545ae027223d56516c92e9edaa8a560e9cd
Miquel Miró Nicolau <miquel.miro@uib.cat>
2023-04-26 15:19:52 +0200

    Changed the version of the package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 LONG_DESCRIPTION = "Package to handle scientific experiments easily"
 
 setup(name='uib_experiments',
-      version='1.5.0',
+      version='1.6.0',
       description='Handle the experiments.',
       long_description=LONG_DESCRIPTION,
       url='https://github.com/miquelmn/uib_experiments',
